bd10dbba91be4acc68781cd8b7df8b5a7eb3eb6e
sudebahadir34 <97553081+sudebahadir34@users.noreply.github.com>
2022-03-30 15:54:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let corbaCesitleri=["mercimek ","15","ezogelin","13","tarhana","18"];
+let anaYemikler=["urfa","45","iskender","50","lahmacun","20"];
+let tatlılar=["künefe","25","sütlaç","10","kadayıf","25"];
